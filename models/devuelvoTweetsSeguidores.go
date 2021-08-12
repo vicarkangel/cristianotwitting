2413b68a5ebe20a8b486a42936c41a4e006005e5
vicarkangel <vicarkangel@hotmail.com>
2021-08-12 11:37:05 -0400

    ultimo end point

---
+++ models/devuelvoTweetsSeguidores.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/* DevuelvoTweets es la estructura con la que devolveremos los Tweests */
+type DevuelvoTweetsSeguidores struct {
+	ID                primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UsuarioID         string             `bson:"usuarioid" json:"usuarioId,omitempty"`
+	UsuarioRelacionID string             `bson:"usuariorelacionid" json:"usuarioRelationId,omitempty"`
+	Tweet             struct {
+		Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
+		Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
+		ID      string    `bson:"fecha" json:"fecha,omitempty"`
+	}
+}
---
+++ routers/leoTweetsRelacion.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/vicarkangel/cristianotwitting/bd"
+)
+
+/* BuscoPerfil busca un perfil en la bd */
+
+func LeoTweetsSeguidores(w http.ResponseWriter, r *http.Request) {
+
+	if len(r.URL.Query().Get("pagina")) < 1 {
+		http.Error(w, "Debe enviar el parametro página", http.StatusBadRequest)
+		return
+	}
+	pagina, err := strconv.Atoi(r.URL.Query().Get("pagina"))
+	if err != nil {
+		http.Error(w, "Debe enviar el parámetro página como entero mayor a 0", http.StatusBadRequest)
+		return
+	}
+
+	respuesta, correcto := bd.LeoTweetsSeguidores(IDUsuario, pagina)
+	if correcto == false {
+		http.Error(w, "Error al leer los tweets", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(respuesta)
+}
